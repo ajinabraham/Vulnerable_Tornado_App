cb4cf360780bc2f6f30ef35d05979e7a9bd61619
Ajin Abraham <ajin25@gmail.com>
2017-05-16 19:45:32 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 """
 Intentionally Vulnerable Web Application
 """
+import immunio.start
 import os
 import io
 import mimetypes
@@ -152,9 +153,9 @@ def main():
     # create_db()
     applicaton = Application()
     http_server = tornado.httpserver.HTTPServer(applicaton)
-    http_server.bind(7776, address='127.0.0.1')
+    http_server.bind(8002, address='127.0.0.1')
     http_server.start()
-    print "Server Started: http://127.0.0.1:7776"
+    print "Server Started: http://127.0.0.1:8002"
     tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == "__main__":
